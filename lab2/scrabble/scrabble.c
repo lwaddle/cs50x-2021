16cd23251ea5041e2c9521086cf31dacbe697d59
Loren Waddle <lwaddle@gmail.com>
2021-05-17 09:50:05 -0700

    Reomve TODO comments

--- lab2/scrabble/scrabble.c
+++ lab2/scrabble/scrabble.c
@@ -5,7 +5,10 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-int ASCII_UPPER_ALPHA[26];    // 26 letters in the alphabet
+
+// Array that will hold 26 letters of the alphabet (uppercase)
+// Uses populate_ascii_upper_alpha() to populate itself.
+int ASCII_UPPER_ALPHA[26];
 
 int compute_score(string word);
 int get_char_index(char c, int array[]);
@@ -24,7 +27,7 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Print the winner
     if (score1 > score2)
     {
         printf("Player 1 wins!\n");
@@ -44,7 +47,7 @@ int main(void)
  */
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    // Compute and return score for string
     int score = 0;
 
     /**
